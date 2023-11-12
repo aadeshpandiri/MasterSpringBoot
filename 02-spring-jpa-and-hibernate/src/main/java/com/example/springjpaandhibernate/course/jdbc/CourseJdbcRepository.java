1c6e6d9b8a3a1e962a492431a9d1f3c03a692cf0
aadesh <aadeshpandiri@gmail.com>
2023-11-12 19:41:48 +0530

    Insert Rows into course table with SPring JDBC

---
+++ 02-spring-jpa-and-hibernate/src/main/java/com/example/springjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,26 @@
+package com.example.springjpaandhibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class CourseJdbcRepository {
+
+    @Autowired
+    private JdbcTemplate springJdbcTemplate;
+
+    private static String INSERT_QUERY =
+            """
+                insert into course (id, name, author)
+                values(1, 'Learn Spring','Ranga');
+    
+            """;
+
+    public void insert(){
+        springJdbcTemplate.update(INSERT_QUERY);
+    }
+
+}
