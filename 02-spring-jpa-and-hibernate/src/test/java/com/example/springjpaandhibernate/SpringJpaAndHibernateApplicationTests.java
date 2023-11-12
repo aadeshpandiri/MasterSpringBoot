c14cf770c91ffa64b4a07ebfa59c4b11925e8b35
aadesh <aadeshpandiri@gmail.com>
2023-11-12 18:45:54 +0530

    spring-jpa-and-hibernate intial code

---
+++ 02-spring-jpa-and-hibernate/src/test/java/com/example/springjpaandhibernate/SpringJpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjpaandhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
